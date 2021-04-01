a994d66f612859bae5871348b68c7da3d791e2d5
Allen <allentango@163.com>
2021-04-01 14:13:15 +0800

    添加 下拉刷新 功能 修改展开详情 ui

--- src/App.js
+++ src/App.js
@@ -1,56 +1,67 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import InfiniteScroll from "react-infinite-scroll-component";
 import Loading from "./components/Loading";
 import Post from "./components/Post";
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [page, setPage] = useState(1);
   const [posts, setPosts] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
 
-  const url =
-    "https://api.github.com/repos/AllenTango/allentango.github.io/issues";
+  let url = `https://api.github.com/repos/AllenTango/allentango.github.io/issues?page=${page}&per_page=10`;
+
+  const loadPost = () => {
+    axios.get(url).then((res) => {
+      setPosts(res.data);
+      setPage(page + 1);
+    });
+  };
+
+  const morePost = () => {
+    axios.get(url).then((res) => {
+      if (res.data.length === 0) {
+        setHasMore(false);
+        return;
+      }
+      setPosts([...posts, ...res.data]);
+      setPage(page + 1);
+    });
+  };
 
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          setIsLoaded(true);
-          setPosts(data);
-        },
-        (error) => {
-          console.log(error);
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+    loadPost();
   }, []);
 
   return (
     <>
       <Header />
       <Main>
-        {!isLoaded ? (
-          <Loading />
-        ) : error ? (
-          <div>Error: {error.message}</div>
-        ) : (
-          <>
-            {posts.map((post) => (
-              <Post
-                key={post["id"]}
-                title={post["title"]}
-                body={post["body"]}
-                labels={post["labels"]}
-                created_time={post["created_at"]}
-                origin_url={post["html_url"]}
-              />
-            ))}
-          </>
-        )}
+        <InfiniteScroll
+          dataLength={posts.length}
+          next={morePost}
+          hasMore={hasMore}
+          loader={<Loading />}
+          endMessage={
+            <div className="text-center text-pink-500 font-bold border-b-4 border-dashed border-pink-400">
+              我是有底线的！^_^
+            </div>
+          }
+        >
+          {posts.map((post) => (
+            <Post
+              key={post["id"]}
+              title={post["title"]}
+              body={post["body"]}
+              labels={post["labels"]}
+              created_time={post["created_at"]}
+              origin_url={post["html_url"]}
+            />
+          ))}
+        </InfiniteScroll>
       </Main>
       <Footer />
     </>
--- src/components/Post.js
+++ src/components/Post.js
@@ -21,7 +21,7 @@ const renderers = {
 function Post({ title, body, labels, created_time, origin_url }) {
   const [showDetail, setShowDetail] = useState(false);
   return (
-    <div className="post" onClick={() => setShowDetail(!showDetail)}>
+    <div className="post">
       <h2 className="post-title">{title}</h2>
       <div className="flex justify-between">
         <div>
@@ -46,8 +46,46 @@ function Post({ title, body, labels, created_time, origin_url }) {
         <ReactMarkdown
           plugins={[gfm]}
           renderers={renderers}
-          children={showDetail ? body : body.slice(0, 150) + "..."}
+          children={showDetail ? body : body.slice(0, 80) + "..."}
         />
+      </div>
+      <div
+        className="text-pink-500 cursor-pointer flex justify-center items-start border-t-4 rounded-b-2xl mt-2 shadow-lg"
+        onClick={() => setShowDetail(!showDetail)}
+      >
+        {!showDetail ? (
+          <div className="down h-12 w-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </div>
+        ) : (
+          <div className="up h-12 w-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M5 15l7-7 7 7"
+              />
+            </svg>
+          </div>
+        )}
       </div>
     </div>
   );
